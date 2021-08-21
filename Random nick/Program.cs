dbf2ccc55d77f3698518b5778b4bb533a6d20fda
anastasia-lab <anastasiya.shpilevaia@ictis.sfedu.ru>
2021-08-21 17:19:02 +0300

    Добавьте файлы проекта.

---
+++ Random nick/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using Random_nick.DataBased;
+
+namespace Random_nick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBaseCharactersDataContext _dataBased = new DataBaseCharactersDataContext();
+            Random rnd = new Random();
+
+            Console.Write("Здравствуйте :) \n");
+            Console.Write("Данная программа может подобрать для Вас никнейм.\n");
+            Console.Write("Для продолжения выберите один из вариантов: ");
+            Console.Write("1 - сгенирировать никнейм; 2 - выход\n");
+            Console.Write("Ваш выбор: ");
+
+            try
+            {
+                var result = Convert.ToInt32(Console.ReadLine());
+
+                if (result == 1)
+                {
+                    var _firstName = _dataBased.FirstNameCharacter.Select(f => f.firstname).ToList();
+                    var _lastName = _dataBased.LastNameCharacteer.Select(l => l.lastname).ToList();
+
+                    string _firstWord = "";
+                    string _lastWord = "";
+
+                    for (int i = 0; i < 1; i++)
+                    {
+                        var _word = rnd.Next(0, _firstName.Count);
+                        _firstWord += _firstName[_word];
+                    }
+
+                    for (int i = 0; i < 1; i++)
+                    {
+                        var word = rnd.Next(0, _lastName.Count);
+                        _lastWord += _lastName[word];
+
+                    }
+
+                    Console.Write("\nНикнейм: ");
+                    Console.WriteLine(_firstWord + "__" + _lastWord);
+                }
+
+                if (result == 2)
+                {
+                    Environment.Exit(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Random nick/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Random nick")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Random nick")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c5b7e19a-82d0-4168-9f68-8b2d3680f9ff")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
